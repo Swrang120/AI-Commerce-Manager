8abc12619d336f6d6236a361ae3e8a40398deb99
Swrang Swargiary <sboro3235@gmail.com>
2026-09-26 03:11:57 +0530

    feat: finish E-commerce Shoping branding

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -16,6 +16,6 @@ class ExampleRobolectricTest {
     fun `read string from context`() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         val appName = context.getString(R.string.app_name)
-        assertEquals("Nexus AI Commerce", appName)
+        assertEquals("E-commerce Shoping", appName)
     }
 }
